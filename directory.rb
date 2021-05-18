c0c130e89f4ffa1f2407b87f459693c841e44b92
Chuka Okoye <chukanorth@hotmail.co.uk>
2021-05-18 13:18:02 +0100

    Added mmethods

--- directory.rb
+++ directory.rb
@@ -12,11 +12,23 @@ students = [
 	"Joffery Baratheon",
 	"Norman Bates"
 ]
-# adn then print them
-puts "The students of Villains Academy"
-puts "-------------"
-students.each do |student|
-	puts student
+
+def print_header
+	puts "The students of Villains Academy"
+	puts "-------------"
+end
+
+def print(names)
+	names.each do |name|
+		puts name
+	end
+end
+
+def print_footer(names)
+	puts "Overall, we have #{names.count} great students"
 end	
-# finally, we print the total number of students
-puts "Overall, we have #{students.count} great students"
+
+#nothing happens until we call the methods
+print_header
+print(students)
+print_footer(students)
